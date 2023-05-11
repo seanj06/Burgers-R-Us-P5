48ba49a12800ec344e33968e3a336f13dc984f97
seanj06 <seanj06@hotmail.com>
2023-05-11 14:31:55 +0000

    Add homepage view and url path

--- home/views.py
+++ home/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+
+def index(request):
+    """ A view to return the home page """
+
+    return render(request, 'home/index.html')
