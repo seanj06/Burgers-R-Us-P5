e8ba538efe24343c2b86c56c0ac73038302c3c37
seanj06 <seanj06@hotmail.com>
2023-05-19 18:48:28 +0000

    Add search functionality for food products

--- products/views.py
+++ products/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 from .models import Food
+from django.contrib import messages
+from django.db.models import Q
 
 # Create your views here.
 
@@ -7,8 +9,26 @@ from .models import Food
 def all_products(request):
 
     products = Food.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Search must not be blank")
+                return redirect(reverse('products'))
+
+            queries = Q(
+                name__icontains=query
+                ) | Q(
+                description__icontains=query
+                )
+
+            products = products.filter(queries)
+
     context = {
         'products': products,
+        'search_term': query,
     }
 
     return render(request, 'products/products.html', context)
