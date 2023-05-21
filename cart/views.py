c8d814425dc58cd18506c6b752e92cd67cfa65c4
seanj06 <seanj06@hotmail.com>
2023-05-21 15:32:52 +0000

    Create cart url, view and add to main url file

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def cart_home(request):
+    """
+    A view that renders the cart page
+    """
+
+    return render(request, 'cart/cart.html')
