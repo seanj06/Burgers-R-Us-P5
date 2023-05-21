124c6a1b06f210feda0a0bc0a57fb6f50a5e2998
seanj06 <seanj06@hotmail.com>
2023-05-21 21:36:32 +0000

    Add add_to_cart view, url and form to product detail template

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cart_home, name='cart_home')
+    path('', views.cart_home, name='cart_home'),
+    path('add/<item_id>/', views.add_to_cart, name='add_to_cart'),
 ]
