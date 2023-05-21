16259d8566aa4b9b77b6b06729d8fdfe0bac66c8
seanj06 <seanj06@hotmail.com>
2023-05-21 21:19:14 +0000

    Add contexts file and context processor to settings

--- burgers/settings.py
+++ burgers/settings.py
@@ -83,6 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.contexts.cart_contents',
             ],
             "builtins": [
                 "crispy_forms.templatetags.crispy_forms_tags",
@@ -212,3 +213,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+
+FREE_DELIVERY_THRESHOLD = 10
+STANDARD_DELIVERY_PERCENTAGE = 10
