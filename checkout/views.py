20a461b85759062ea81df2049c8c6076414dbda6
seanj06 <seanj06@hotmail.com>
2023-05-27 12:00:55 +0000

    Add stripe payment intent to checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -9,21 +9,37 @@ import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
 
     if not cart:
         messages.error(request, "Your cart is empty")
         return redirect(reverse('products'))
-    
+
     current_cart = cart_contents(request)
     total = current_cart['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    print(intent)
+
     order_form = OrderForm()
+    
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51N1pgCIlTPUC1zeufzd2LYcj8SxO8AaT2kLgB6tZR32ks1a0td3ISGgkjrljcATS2gwhWncQ2q56NsunQKdauSvb00hKYa4lR2',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
