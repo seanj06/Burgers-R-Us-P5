5c34a26ff0b8214634222fcbb0cb70abe6490f74
seanj06 <seanj06@hotmail.com>
2023-05-27 12:10:27 +0000

    Add javascript for checkout functionality

--- checkout/views.py
+++ checkout/views.py
@@ -30,7 +30,7 @@ def checkout(request):
     print(intent)
 
     order_form = OrderForm()
-    
+
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
