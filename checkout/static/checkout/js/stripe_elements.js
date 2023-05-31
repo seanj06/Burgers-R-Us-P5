cafb1998c030872c44414c43e3d7067f286edade
seanj06 <seanj06@hotmail.com>
2023-05-31 12:03:50 +0000

    Add event listener in element.js and cache data url

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -55,49 +55,70 @@ form.addEventListener('submit', function (ev) {
         'disabled': true
     });
     $('#submit-button').attr('disabled', true);
-    stripe.confirmCardPayment(clientSecret, {
-        payment_method: {
-            card: card,
-            billing_details: {
-                name: $.trim(form.full_name.value),
-                email: $.trim(form.email.value),
-                address: {
-                    line1: $.trim(form.address_1.value),
-                    line2: $.trim(form.address_2.value),
-                    city: $.trim(form.address_3.value),
-                    country: $.trim(form.county.value),
-                    state: $.trim(form.town.value),
+    $('#payment-form').fadeToggle(100);
+    $('#loading-overlay').fadeToggle(100);
+
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // From using {% csrf_token %} in the form
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    email: $.trim(form.email.value),
+                    address: {
+                        line1: $.trim(form.address_1.value),
+                        line2: $.trim(form.address_2.value),
+                        line3: $.trim(form.address_3.value),
+                        county: $.trim(form.county.value),
+                        town: $.trim(form.town.value),
+                    }
                 }
             },
             shipping: {
                 name: $.trim(form.full_name.value),
+                phone: $.trim(form.phone_number.value),
                 address: {
                     line1: $.trim(form.address_1.value),
                     line2: $.trim(form.address_2.value),
                     city: $.trim(form.address_3.value),
-                    country: $.trim(form.town.value),
-                    postal_code: $.trim(form.eircode.value),
-                    state: $.trim(form.county.value),
+                    county: $.trim(form.county.value),
+                    eircode: $.trim(form.eircode.value),
+                    town: $.trim(form.town.value),
                 }
             },
-        }
-    }).then(function (result) {
-        if (result.error) {
-            var errorDiv = document.getElementById('card-errors');
-            var html = `
-                <span class="icon" role="alert">
-                <i class="fas fa-times"></i>
-                </span>
-                <span>${result.error.message}</span>`;
-            $(errorDiv).html(html);
-            card.update({
-                'disabled': false
-            });
-            $('#submit-button').attr('disabled', false);
-        } else {
-            if (result.paymentIntent.status === 'succeeded') {
-                form.submit();
+        }).then(function (result) {
+            if (result.error) {
+                var errorDiv = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorDiv).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-overlay').fadeToggle(100);
+                card.update({
+                    'disabled': false
+                });
+                $('#submit-button').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
             }
-        }
-    });
+        });
+    }).fail(function () {
+        // just reload the page, the error will be in django messages
+        location.reload();
+    })
 });
