b6c71d9dd04e231770eb6112a4453df0ba9e79a7
seanj06 <seanj06@hotmail.com>
2023-06-01 12:55:50 +0000

    Add form validation logic

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import Profile
 from .forms import ProfileForm
 
 
 def profile(request):
-    
+
     profile = get_object_or_404(Profile, user=request.user)
     form = ProfileForm(instance=profile)
     orders = profile.orders.all()
+
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your Profile has been updated')
+        else:
+            messages.error(request, 'Sorry something went wrong')    
+
     template = 'profiles/profile.html'
     context = {
         'form': form,
