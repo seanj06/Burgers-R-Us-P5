3715862a9da6fd5f5dd7914610ec288f885ea1dd
seanj06 <seanj06@hotmail.com>
2023-06-10 13:42:50 +0000

    Create ContactMessageForm

---
+++ contact/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import ContactMessage
+
+
+class ContactMessageForm(forms.ModelForm):
+    """
+    Form for ContactMessage Model
+    """
+    class Meta:
+        model = ContactMessage
+        fields = ['name', 'email', 'subject', 'message', 'issue']
+        widgets = {
+            'name': forms.TextInput(attrs={'placeholder': 'Your Name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Your Email'}),
+            'subject': forms.TextInput(attrs={'placeholder': 'Subject'}),
+            'message': forms.Textarea(attrs={'placeholder': 'Your Message'}),
+            'issue': forms.Select(attrs={'class': 'custom-select'}),
+        }
