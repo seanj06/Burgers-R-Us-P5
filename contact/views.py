c768818f8b5794eaf78e7f933b3798c899a56ca0
seanj06 <seanj06@hotmail.com>
2023-06-10 13:49:59 +0000

    Add view code to render contact form

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from django.contrib import messages
+from .forms import ContactMessageForm
 
 
 def contact(request):
     """ A view to return the contact page """
+    if request.method == 'POST':
+        form = ContactMessageForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, 'Your form has been submitted,\
+                     we will be in touch shortly'
+                )
+            return redirect('home')
+    else:
+        form = ContactMessageForm()
 
     return render(request, 'contact/contact.html')
