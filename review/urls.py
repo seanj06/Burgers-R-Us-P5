3720e4782f154651fbdb9c85c7cc30327e509868
seanj06 <seanj06@hotmail.com>
2023-06-12 20:57:07 +0000

    Add view logic and javascript for like button

--- review/urls.py
+++ review/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.reviews, name='reviews'),
+    path("like/", views.like_review, name="like_review"),
 ]
--- review/views.py
+++ review/views.py
@@ -14,3 +14,23 @@ def reviews(request):
         'reviews': reviews,
     }
     return render(request, 'review/review.html', context)
+
+
+def like_review(request):
+    """
+    View for handling review like logic
+    """
+    if request.method == "POST" and request.is_ajax():
+        review_id = request.POST.get("review_id")
+        review = get_object_or_404(Review, id=review_id)
+        if review.likes.filter(id=request.user.id).exists():
+            review.likes.remove(request.user)
+        else:
+            review.likes.add(request.user)
+
+        # Return the updated like count
+        likes_count = review.likes_count()
+        return JsonResponse({"likes_count": likes_count})
+    else:
+        return JsonResponse({}, status=400)
+
