56a3f077deff6364a509e21c8e90cfa0c79828bd
seanj06 <seanj06@hotmail.com>
2023-06-12 12:56:06 +0000

    Add review view, url and template

--- review/views.py
+++ review/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Review
 
 # Create your views here.
+
+
+def reviews(request):
+    """
+    View to render a list of all reviews
+    """
+    reviews = Review.objects.all()
+    context = {
+        'reviews': reviews,
+    }
+    return render(request, 'review/review_list.html', context)
