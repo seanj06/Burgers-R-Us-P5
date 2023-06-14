2748c7410fcf10ec48922642f6db5724a8916465
seanj06 <seanj06@hotmail.com>
2023-06-14 12:40:07 +0000

    Add template and javascript code for delete review

--- review/urls.py
+++ review/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path('', views.reviews, name='reviews'),
     path("like/", views.like_review, name="like_review"),
     path('add/', views.add_review, name='add_review'),
-    path('delete/', views.add_review, name='delete_review'),
+    path('delete/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
