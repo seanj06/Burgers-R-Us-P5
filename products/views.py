e5486fa6fef1eaaa3026203d25ad2c7df983bb75
seanj06 <seanj06@hotmail.com>
2023-06-18 12:08:53 +0000

    Add edit product view and url

--- products/views.py
+++ products/views.py
@@ -104,7 +104,9 @@ def product_detail(request, product_id):
 
 @login_required
 def add_product(request):
-    """ Add a product to the store """
+    """
+    Add a product to the store
+    """
     if not request.user.is_superuser:
         messages.error(request,
                        'Sorry you are not authorized to add a product'
@@ -130,3 +132,38 @@ def add_product(request):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def edit_product(request, product_id):
+    """
+    Edit a product in the store
+    """
+    if not request.user.is_superuser:
+        messages.error(request,
+                       'Sorry you are not authorized to add a product'
+                       )
+        return redirect(reverse('home'))
+    product = get_object_or_404(Food, pk=product_id)
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated product!')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request,
+                           'Failed to update product.\
+                            Please ensure the form is valid.'
+                           )
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are editing {product.name}')
+
+    template = 'products/edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)    
