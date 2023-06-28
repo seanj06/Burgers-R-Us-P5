1fbb274f7edcc5f3784cc2305638042cb5c523c1
seanj06 <seanj06@hotmail.com>
2023-06-28 12:27:09 +0000

    Add code to send contact confirmation email

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import ContactMessageForm
+from .models import ContactMessage
 from django.contrib.auth.decorators import login_required
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 
 @login_required
@@ -29,4 +33,16 @@ def contact_success(request):
     """
     View to render contact success page
     """
+    contact = ContactMessage.objects.latest('time')
+    subject = f'Enquiry Confirmation - Enquiry #{contact.subject}'
+    from_email = 'burgers-r-us@example.com'
+    to_email = [contact.email]
+
+    html_message = render_to_string('contact/email/enquiry_confirmation.html',
+                                    {'contact': contact})
+    plain_message = strip_tags(html_message)
+
+    send_mail(subject, plain_message,
+              from_email, to_email,
+              html_message=html_message)
     return render(request, 'contact/contact-success.html')
