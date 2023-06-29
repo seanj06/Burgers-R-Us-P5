13ead548eb161eac2e3ff283fb6c287dc05a1970
seanj06 <seanj06@hotmail.com>
2023-06-29 14:46:00 +0000

    Add unit test for remove_from_cart view

--- cart/test_views.py
+++ cart/test_views.py
@@ -11,12 +11,18 @@ class TestCartHome(TestCase):
     Unit Test for cart_home view
     """
     def setUp(self):
+        """
+        Setup method
+        """
         self.url = reverse('cart_home')
         self.user = User.objects.create_user(
             username='testuser', password='testpassword'
             )
 
     def test_cart_home_view(self):
+        """
+        Test for view
+        """
         self.client.login(username='testuser', password='testpassword')
 
         response = self.client.get(self.url)
@@ -31,6 +37,9 @@ class TestAddToCart(TestCase):
     Unit Test for add_to_cart view
     """
     def setUp(self):
+        """
+        Setup method
+        """
         self.client = Client()
         self.user = User.objects.create_user(
             username='testuser', password='testpassword'
@@ -38,6 +47,9 @@ class TestAddToCart(TestCase):
         self.product = Food.objects.create(name='Test Food', price=10)
 
     def test_add_to_cart_view(self):
+        """
+        Test for view
+        """
         self.client.login(username='testuser', password='testpassword')
 
         item_id = self.product.pk
@@ -53,3 +65,63 @@ class TestAddToCart(TestCase):
         self.assertRedirects(response, redirect_url)
         cart = self.client.session.get('cart', {})
         self.assertEqual(cart.get(str(item_id)), quantity)
+
+
+class TestEditCart(TestCase):
+    """
+    Unit Test for edit_cart view
+    """
+    def setUp(self):
+        """
+        Setup method
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+            )
+        self.product = Food.objects.create(name='Test Food', price=10)
+        self.cart = {'1': 2}
+        self.client.session['cart'] = self.cart
+
+    def test_edit_cart_view(self):
+        """
+        Test for view
+        """
+        self.client.login(username='testuser', password='testpassword')
+        item_id = self.product.pk
+        quantity = 3
+        response = self.client.post(reverse('edit_cart', args=[item_id]),
+                                    {'quantity': quantity}
+                                    )
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('cart_home'))
+        updated_cart = self.client.session.get('cart', {})
+        self.assertEqual(updated_cart.get(str(item_id)), quantity)
+
+
+class TestRemoveFromCart(TestCase):
+    """
+    Unit Test for remove_from_cart view
+    """
+    def setUp(self):
+        """
+        Setup method
+        """
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='testuser', password='testpassword'
+            )
+        self.product = Food.objects.create(name='Test Food', price=10)
+        self.cart = {'1': 2}
+        self.client.session['cart'] = self.cart
+
+    def test_remove_from_cart_view(self):
+        """
+        Test for view
+        """
+        self.client.login(username='testuser', password='testpassword')
+        item_id = self.product.pk
+        response = self.client.post(reverse('remove_from_cart', args=[item_id]))
+        self.assertEqual(response.status_code, 200)
+        updated_cart = self.client.session.get('cart', {})
+        self.assertNotIn(str(item_id), updated_cart)
