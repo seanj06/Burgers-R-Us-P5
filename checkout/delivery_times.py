1c009f8147e10079bcd5a640f497d6f37b1b9f5a
seanj06 <seanj06@hotmail.com>
2023-07-06 20:19:29 +0000

    Change checkout delivery times

--- checkout/delivery_times.py
+++ checkout/delivery_times.py
@@ -4,10 +4,10 @@ from datetime import datetime, timedelta, time
 DELIVERY_TIME_DELAY = 30
 
 OPENING_HOURS = {
-    0: {'start': '11:00', 'end': '21:00'},   # Monday
-    1: {'start': '11:00', 'end': '21:00'},   # Tuesday
-    2: {'start': '11:00', 'end': '21:00'},   # Wednesday
-    3: {'start': '11:00', 'end': '21:00'},   # Thursday
+    0: {'start': '11:00', 'end': '23:00'},   # Monday
+    1: {'start': '11:00', 'end': '23:00'},   # Tuesday
+    2: {'start': '11:00', 'end': '23:00'},   # Wednesday
+    3: {'start': '11:00', 'end': '23:00'},   # Thursday
     4: {'start': '12:00', 'end': '01:30'},   # Friday
     5: {'start': '12:00', 'end': '02:30'},   # Saturday
     6: {'start': '12:00', 'end': '00:30'},   # Sunday
