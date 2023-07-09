68c1531154c715eb6d10c5707789cfd26cfd84e4
seanj06 <seanj06@hotmail.com>
2023-07-09 12:39:21 +0000

    Remove duplicate js code from cart.js

--- cart/static/cart/js/cart.js
+++ cart/static/cart/js/cart.js
@@ -1,22 +1,4 @@
-$('.update-link').click(function (e) {
-    var form = $(this).prev('.update-form');
-    form.submit();
-});
 
-// Remove item and reload on click
-$('.remove-item').click(function (e) {
-    var csrfToken = "{{ csrf_token }}";
-    var itemId = $(this).attr('id').split('remove_')[1];
-    var url = `/cart/remove/${itemId}/`;
-    var data = {
-        'csrfmiddlewaretoken': csrfToken,
-    };
-
-    $.post(url, data)
-        .done(function () {
-            location.reload();
-        });
-});
 
 $('#sort-selector').change(function () {
     var selector = $(this);
